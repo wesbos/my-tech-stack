ba1293e99a444b6d4580748cc58386f647126b38
Jacob Smith <jacob.smith@changehealthcare.com>
2020-04-23 21:19:37 -0500

    Base page configuration and styling

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
-import Layout from '../components/Layout';
 import Link from 'next/link';
+import Head from 'next/head';
 import fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+
+import utilStyles from '../styles/utils.module.css';
+
+// add attributes for Link at the <a> tag level
 
 const Index = props => {
   return (
-    <Layout>
-      <h1>Batman TV Shows</h1>
+    <Layout home>
+      {/* <h1>Batman TV Shows</h1>
       <ul>
         {props.shows.map(show => (
           <li key={show.id}>
             <Link href="/p/[id]" as={`/p/${show.id}`}>
-              <a>{show.name}</a>
+              <a className="example--class">{show.name}</a>
             </Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
+      <div className={utilStyles.typewriter}>
+        <h2>Welcome Friends!</h2>
+      </div>
+      {/* <div>
+        <Link href="posts/first-post">
+          <a>First Post</a>
+        </Link>
+      </div> */}
     </Layout>
   )
 };
@@ -22,10 +35,8 @@ const Index = props => {
 Index.getInitialProps = async () =>  {
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
   const data = await res.json();
-
   console.log(`Show data fetched. Count: ${data.length}`);
-  console.log(`Show data fetched `, {data});
-
+  console.log(`Show data fetched `, data.map(entry => entry));
   return {
     shows: data.map(entry => entry.show)
   };
---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Layout from '../../components/Layout';
+import Head from 'next/head';
+
+const FirstPost = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+    </Layout>
+  )
+}
+
+export default FirstPost
