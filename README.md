e05500069524ecc81c0bcf548eaf56933d155cee
Jacob Smith <jacob.smith@changehealthcare.com>
2020-04-23 22:14:05 -0500

    Simple getStaticProps implementation to read md and pre-render data

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## A personal website built with Next.js to showcase:
 1. My technical skills
-1. Add side projects
+1. Showcase side projects
 1. Ways to contact me
 1. Write blogs about new tech or walkthroughs on a particular topic
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,51 @@ import Link from 'next/link';
 import Head from 'next/head';
 import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
+import { getSortedPostsData } from '../lib/posts';
 
 import utilStyles from '../styles/utils.module.css';
 
 // add attributes for Link at the <a> tag level
 
-const Index = props => {
+const Index = ({ allPosts }) => {
   return (
     <Layout home>
-      {/* <h1>Batman TV Shows</h1>
+      <div className={utilStyles.typewriter}>
+        <h2>Welcome Friends!</h2>
+      </div>
+      <br/>
+      <h3>Blog</h3>
+      <ul className={utilStyles.listItem}>
+        {allPosts.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            {title} <br/> {date}
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+};
+
+export async function getStaticProps() {
+  const allPosts = getSortedPostsData();
+  return {
+    props: {
+      allPosts
+    }
+  }
+}
+
+
+// Index.getInitialProps = async () =>  {
+//   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//   const data = await res.json();
+//   console.log(`Show data fetched. Count: ${data.length}`);
+//   console.log(`Show data fetched `, data.map(entry => entry));
+//   return {
+//     shows: data.map(entry => entry.show)
+//   };
+
+        {/* <h1>Batman TV Shows</h1>
       <ul>
         {props.shows.map(show => (
           <li key={show.id}>
@@ -20,26 +56,8 @@ const Index = props => {
           </li>
         ))}
       </ul> */}
-      <div className={utilStyles.typewriter}>
-        <h2>Welcome Friends!</h2>
-      </div>
-      {/* <div>
-        <Link href="posts/first-post">
-          <a>First Post</a>
-        </Link>
-      </div> */}
-    </Layout>
-  )
-};
-
-Index.getInitialProps = async () =>  {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
-  console.log(`Show data fetched. Count: ${data.length}`);
-  console.log(`Show data fetched `, data.map(entry => entry));
-  return {
-    shows: data.map(entry => entry.show)
-  };
-};
+// };
 
 export default Index;
+
+
