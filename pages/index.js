938390c29f64318081131d645fc7da1c4f0c926e
Jacob Smith <jacob.smith@changehealthcare.com>
2020-05-04 21:28:09 -0500

    Resizing img

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,10 @@ const Index = ({ allPosts }) => {
   return (
     <Layout home title={title} >
         <h1 className='text-6xl'>Welcome!</h1>
-      <section className='flex flex-row-reverse justify-around'>
-        <img className='rounded-full w-2/6' src='/images/profile.jpg'/>
+      <section className='flex flex-row-reverse items-center'>
+      <span className='w-2/6'>
+          <img className='rounded-full w-2/3' src='/images/profile.jpg'/>
+        </span>
         <article className='leading-10 w-4/6 mr-8 text-justify'>
         Info about Me 
         ↪ Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32
